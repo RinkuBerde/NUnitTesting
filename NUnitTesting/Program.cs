e1a9fdf5d6242aea0d8827dabb1de67405ca171b
rinkuberde <rinkuberde@gmail.com>
2023-01-21 22:24:27 +0530

    [Rinku] - dayOfWeek Program

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -5,6 +5,21 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to  NUnit Testing Program!!!");
+            Console.WriteLine("\t\t\t\t Programs for N-Unit Testing");
+            Console.WriteLine("1. Find the day of the week");
+            Console.WriteLine("------------------------------------------------------------------");
+            Console.Write("Enter the option : ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            dayOfWeek nunit = new dayOfWeek();
+            switch (option)
+            {
+                case 1:
+                    dayOfWeek.CalculateDay();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter from the given choice");
+                    break;
+            }
         }
     }
 }
