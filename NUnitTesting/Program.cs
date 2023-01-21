86a953526ce4f78446a9c46145aa788b42961872
rinkuberde <rinkuberde@gmail.com>
2023-01-21 22:14:28 +0530

    [Rinku] - Welcome Message

---
+++ NUnitTesting/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NUnitTesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to  NUnit Testing Program!!!");
+        }
+    }
+}
