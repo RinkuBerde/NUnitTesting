3a73bd0b5bb95cd68ebe20ed6c591a265e8c4f22
rinkuberde <rinkuberde@gmail.com>
2023-01-21 23:51:21 +0530

    [Rinku] - Temperatur Conversion

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -7,6 +7,7 @@
             Console.WriteLine("Welcome to  NUnit Testing Program!!!");
             Console.WriteLine("\t\t\t\t Programs for N-Unit Testing");
             Console.WriteLine("1. Find the day of the week");
+            Console.WriteLine("2. Convert the temperature to either Fahrenheit/Celcius");
             Console.WriteLine("------------------------------------------------------------------");
             Console.Write("Enter the option : ");
             int option = Convert.ToInt32(Console.ReadLine());
@@ -15,6 +16,9 @@
             {
                 case 1:
                     dayOfWeek.CalculateDay();
+                    break;
+                case 2:
+                    temperaturConversion.CalculateTemperature();
                     break;
                 default:
                     Console.WriteLine("Please Enter from the given choice");
